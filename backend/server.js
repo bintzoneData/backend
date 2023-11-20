05644e2f7877478b72628ff39e05ef072e1132a3
maoo abdalla <maoo@bintzone.com>
2023-11-20 22:26:28 +0300

    addd

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ connectDB();
 const app = express();
 
 // cors
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://bintzone-api.onrender.com'],
+  })
+);
 // getting bodyjson
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
