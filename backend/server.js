6075b3155ec3d9b77089b676b9756e0d9f89b48b
maoo abdalla <maoo@bintzone.com>
2023-11-20 05:49:01 +0300

    all

--- backend/server.js
+++ backend/server.js
@@ -15,28 +15,26 @@ const app = express();
 app.use(cors());
 // getting bodyjson
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 // routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
 app.use('/api/items', require('./routes/itemRoute'));
 app.use('/api/categories', require('./routes/categoryRoute'));
 //  serve frontend
-// if (process.env.NODE_ENV === 'production') {
-//   // set build folder as static
-//   app.use(express.static(path.join(__dirname, '../frontend/build')));
-//   // FIX: below code fixes app crashing on refresh in deployment
-//   app.get('*', (_, res) => {
-//     res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-//   });
-// } else {
-//   app.get('/', (req, res) => {
-//     res.status(200).json({ message: 'welcome to bintGO Api' });
-//   });
-// }
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'welcome to bintGO Api' });
-});
+if (process.env.NODE_ENV === 'production') {
+  // set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'welcome to bintGO Api' });
+  });
+}
+
 // errorHandler
 app.use(errorHandler);
 app.listen(PORT, () => console.log(`server started on port${PORT}`));
